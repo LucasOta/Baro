fcd5c072e5b402bf1ce3b3c5d27041d99973ef0b
LucasOta <lucasmatiasota@gmail.com>
2020-12-11 19:57:03 -0300

    Fix Template js imports

--- frontend/src/app/modules/website/website.component.ts
+++ frontend/src/app/modules/website/website.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Inject, OnInit, Renderer2, ViewEncapsulation } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-website',
@@ -15,10 +16,36 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
     encapsulation: ViewEncapsulation.None
 })
 export class WebsiteComponent implements OnInit {
-
-  constructor() { }
+  scripts = [
+    'vendor/jquery-form/dist/jquery.form.min.js',
+    'vendor/object-fit-images/dist/ofi.min.js',
+    'vendor/popper.js/dist/umd/popper.min.js',
+    'vendor/tether/dist/js/tether.min.js',
+    'vendor/bootstrap/dist/js/bootstrap.min.js',
+    'vendor/sticky-kit/dist/sticky-kit.min.js',
+    'vendor/jarallax/dist/jarallax.min.js',
+    'vendor/jarallax/dist/jarallax-video.min.js',
+    'vendor/isotope-layout/dist/isotope.pkgd.min.js',
+    'vendor/imagesloaded/imagesloaded.pkgd.min.js',
+    'vendor/lightgallery/dist/js/lightgallery.min.js',
+    'vendor/lg-fullscreen/dist/lg-fullscreen.min.js',
+    'vendor/lg-video/dist/lg-video.min.js',
+    'vendor/swiper/dist/js/swiper.min.js',
+    'js/mimilism.js',
+    'js/mimilism-init.js',
+  ]
+  constructor(
+    private renderer: Renderer2, 
+    @Inject(DOCUMENT) private document: any
+  ) { }
 
   ngOnInit(): void {
+    this.scripts.forEach(route => {      
+      let scriptElt = this.renderer.createElement('script');
+      this.renderer.setAttribute(scriptElt, 'type', 'text/javascript');
+      this.renderer.setAttribute(scriptElt, 'src', `assets/mimilism/${route}`);
+      this.renderer.appendChild(this.document.body, scriptElt);
+    });
   }
 
 }
