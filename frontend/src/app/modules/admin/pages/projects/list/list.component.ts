f35bae7a0bf69f4b5a94ee329ccad2d76eb41a33
LucasOta <lucasmatiasota@gmail.com>
2020-12-14 00:33:48 -0300

    Take out desc and add thumnail on projects list

--- frontend/src/app/modules/admin/pages/projects/list/list.component.ts
+++ frontend/src/app/modules/admin/pages/projects/list/list.component.ts
@@ -9,6 +9,7 @@ import { Router } from "@angular/router";
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  moduleName = 'projects'; 
   projects: Project[] = [];
 
   constructor(private projectService: ProjectService, private router: Router) { }
