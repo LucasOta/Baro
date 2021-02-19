9aa5e58b877e71d1858155f57b40e37c8be98188
LucasOta <lucasmatiasota@gmail.com>
2021-02-19 20:47:04 -0300

    feat: add template driven form and validation

--- frontend/src/app/modules/website/pages/contact/contact.component.ts
+++ frontend/src/app/modules/website/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from 'src/app/shared/models/contact';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  // form={
+  //   name: '',
+  //   name: '',
+  //   name: '',
+  //   name: '',
+  //   name: '',
+  // };
+  model = new Contact();
 
+  submitted = false;
+  // Seguir desde acá
+  // https://angular.io/guide/forms
+  
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() { 
+    this.submitted = true;
+
+    if(this.model.name && this.model.email && this.model.message){
+      // Send form
+      console.log(this.model);
+    }
+    
   }
+
+
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
 
 }
